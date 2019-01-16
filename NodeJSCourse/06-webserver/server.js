88b6a11c77cad892a177a2d53be2f83683935ac5
Mario Ortega <mcor63@gmail.com>
2019-01-16 16:04:32 -0400

    Se finaliza las practicas con HBS

--- NodeJSCourse/06-webserver/server.js
+++ NodeJSCourse/06-webserver/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 
 var hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
@@ -12,11 +15,17 @@ app.set('view engine', 'hbs');
 app.get('/', (req, res) => {
 
     res.render('home', {
-        nombre: 'Mario',
-        anio: new Date().getFullYear()
+        nombre: 'mario ortega'
     });
 });
 
-app.listen(3000, () => {
-    console.log('Escuchando peticiones en el puerto 3000');
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+
+
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${ port }`);
 })
