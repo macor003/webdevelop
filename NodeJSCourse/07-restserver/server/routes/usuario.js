e8beb5751472c52abc6c227961ab7f612938dbb0
Mario Ortega <mcor63@gmail.com>
2019-01-21 16:32:05 -0400

    Se continua practica con base de datos mongo y metodo POST

--- NodeJSCourse/07-restserver/server/routes/usuario.js
+++ NodeJSCourse/07-restserver/server/routes/usuario.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const Usuario = require('../models/usuario');
 const app = express();
+
 
 app.get('/usuario', function(req, res) {
     res.json('get Usuario');
@@ -9,27 +12,48 @@ app.post('/usuario', function(req, res) {
 
     let body = req.body;
 
-    if (body.nombre === undefined) {
+    let usuario = new Usuario({
+        nombre: body.nombre,
+        email: body.email,
+        password: bcrypt.hashSync(body.password, 10),
+        role: body.role
 
-        res.status(400).json({
-            ok: false,
-            mensaje: "El nombre es necesario"
-        })
+    });
 
-    } else {
+    usuario.save((err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
 
         res.json({
-            data: body
+            ok: true,
+            usuario: usuarioDB
         });
-    }
+
+    });
 });
 
 app.put('/usuario/:id', function(req, res) {
     let id = req.params.id;
+    let body = req.body;
 
-    res.json({
-        id
-    });
+    Usuario.findByIdAndUpdate(id, body, { new: true }, (err, usuarioDB) => {
+
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+    })
+
 });
 
 app.delete('/usuario', function(req, res) {
--- NodeJSCourse/07-restserver/server/server.js
+++ NodeJSCourse/07-restserver/server/server.js
@@ -8,17 +8,18 @@ const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-app.use(require('./routes/usuario'));
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+app.use(require('./routes/usuario'));
 
 
+// Conexión a la base de datos
 mongoose.connect('mongodb://localhost:27017/cafe', { useNewUrlParser: true }, (err, res) => {
     if (err) throw err;
-
     console.log('Base de datos ONLINE');
 });
 
